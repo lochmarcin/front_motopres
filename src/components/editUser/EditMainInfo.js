793ae96227625e7e0b2d11cc0d088db670f00c7c
Marcin Łoch <m.loch@sedziszow.pseap.local>
2022-06-09 15:33:33 +0200

    change user panel

--- src/components/editUser/EditMainInfo.js
+++ src/components/editUser/EditMainInfo.js
@@ -14,38 +14,7 @@ const EditMainInfo = () => {
     }
 
 
-    React.useEffect(() => {
-        let user_id = null
-
-        const idUser = async () => {
-
-            try {
-                await axios.get(Url + '/auth/me').then((response) => {
-                    user_id = response.data.user_id
-                    console.log("response: " + response.data.user_id)
-                    console.log("user_id: " + user_id)
-                });
-            } catch (err) {
-                console.log("Error: from editUserMain, get /auth/me to set iduser" + err)
-            }
-        }
-        const getInfoAboutUser = async () => {
-
-            try {
-                await axios.get(Url + '/users/getOne/' + user_id).then((response) => {
-                    setUserMainInfo(response.data)
-                    console.log("response /users/getOne/: ")
-                    console.log(response.data)
-                });
-            } catch (err) {
-                console.log("Error: from editUserMain, get /auth/me to set iduser" + err)
-            }
-        }
-        idUser()
-            .then(() => {
-                getInfoAboutUser()
-            })
-    }, []);
+    
 
     return (
         <>
@@ -54,17 +23,17 @@ const EditMainInfo = () => {
                 <Table id="mainInfoUserTable" striped bordered hover>
                     <tr>
                         <td>Imie</td>
-                        {/* <td><Form.Control placeholder={userMainInfo.firstname ? userMainInfo.firstname : "brak"} /></td> */}
-                        {/* <td><Form.Control placeholder={userMainInfo.firstname ? userMainInfo.firstname : "brak"} /></td> */}
+                        <td><Form.Control placeholder={userMainInfo.firstname ? userMainInfo.firstname : "brak"} /></td>
+                        <td><Form.Control placeholder={userMainInfo.firstname ? userMainInfo.firstname : "brak"} /></td>
 
                     </tr>
                     <tr>
                         <td>Nazwisko</td>
-                        {/* <td><Form.Control placeholder={userMainInfo.lastname ? userMainInfo.lastname : "brak"} /></td> */}
+                        <td><Form.Control placeholder={userMainInfo.lastname ? userMainInfo.lastname : "brak"} /></td>
                     </tr>
                     <tr>
                         <td>Login:</td>
-                        {/* <td><Form.Control placeholder={userMainInfo.username ? userMainInfo.username : "brak"} /></td> */}
+                        <td><Form.Control placeholder={userMainInfo.username ? userMainInfo.username : "brak"} /></td>
                     </tr>
 
                 </Table>
