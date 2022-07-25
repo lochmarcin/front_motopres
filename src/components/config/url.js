3b30ce13b1bc284c27c64078a2851035a1551acf
Marcin Łoch <m.loch@sedziszow.pseap.local>
2022-07-25 15:04:04 +0200

    url set

--- src/components/config/url.js
+++ src/components/config/url.js
@@ -5,7 +5,7 @@ const production = false
 
 const Url = {
     api : production ? 'http://51.83.134.120:5000': 'http://localhost:5000',
-    front : production ? 'https://51.83.134.120' : 'https://localhost:3000'
+    front : production ? 'http://51.83.134.120' : 'https://localhost:3000'
 }
 
 
