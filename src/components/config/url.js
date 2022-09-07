7b4ae57f0b32169a517faa5e55bae7e52562e19e
Marcin Łoch <m.loch@sedziszow.pseap.local>
2022-09-07 13:43:53 +0200

    url

--- src/components/config/url.js
+++ src/components/config/url.js
@@ -1,11 +1,11 @@
 // const Url = 'http://127.0.0.1:5000'
 // const Url = 'http://51.83.134.120:5000'
 
-const production = true
+const production = false
 
 const Url = {
     api : production ? 'http://51.83.134.120:5000': 'http://localhost:5000',
-    front : production ? 'http://51.83.134.120' : 'https://localhost:3000'
+    front : production ? 'http://51.83.134.120' : 'http://localhost:3000'
 }
 
 
