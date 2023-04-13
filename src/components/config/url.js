13bd3162822e8b4a133a832dad10fc39bb45a0a3
unknown <marcin.loch23@gmail.com>
2023-04-13 23:59:49 +0200

    some changes

--- src/components/config/url.js
+++ src/components/config/url.js
@@ -1,7 +1,7 @@
 // const Url = 'http://127.0.0.1:5000'
 // const Url = 'http://51.83.134.120:5000'
 
-const production = false
+const production = true
 
 const Url = {
     api : production ? 'https://api.motopres.marcinloch.pl': 'http://localhost:5000',
